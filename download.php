ac13b7dbbee60b24688094dcd556a6f80ea17804
kimseyun13579danny <133724434+kimseyun13579danny@users.noreply.github.com>
2023-05-18 10:58:14 +0900

    Update download.php

--- download.php
+++ download.php
@@ -9,7 +9,7 @@
 
 		header("Content-type: application/octet-stream"); 
         header("Content-Length: ".filesize("$file"));
-        header("Content-Disposition: attachment; filename=$filename"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
+        header("Content-Disposition: attachment; filename=$Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
         header("Content-Transfer-Encoding: binary"); 
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
         header("Pragma: public"); 
@@ -18,7 +18,7 @@
 	    if (preg_match("MSIE", $_SERVER['HTTP_USER_AGENT'])) { 
 	        header("Content-type: application/octet-stream"); 
 	        header("Content-Length: ".filesize("$file"));
-	        header("Content-Disposition: attachment; filename=$filename"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
+	        header("Content-Disposition: attachment; filename=$Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
 	        header("Content-Transfer-Encoding: binary"); 
 	        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 	        header("Pragma: public"); 
@@ -26,7 +26,7 @@
 	    }else { 
 	        header("Content-type: file/unknown"); 
 	        header("Content-Length: ".filesize("$file")); 
-	        header("Content-Disposition: attachment; filename=$filename"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
+	        header("Content-Disposition: attachment; filename=$Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent"); // Win11_22H222621.1555_4in1_x64_Remiz_23.04.15.torrent
 	        header("Content-Description: PHP3 Generated Data"); 
 	        header("Pragma: no-cache"); 
 	        header("Expires: 0"); 
